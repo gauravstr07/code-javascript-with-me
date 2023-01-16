5bcb2725c7754e7e3cc0deb8f6079bd2b823d83a
Gaurav Sutar <gauravstr05@gmail.com>
2023-01-16 18:01:34 +0530

    Basics of Array

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 let marks = [80, 90, 50, 60, 75, 95, 98, 85, 80, null, "Not Present"];
+
+marks[11] = 65; // adding a new value to the array
+
 console.log(marks);
+console.log(marks.length); // to find indexes in array
