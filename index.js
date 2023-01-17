4aa6aa19ec4f56b6ff854944e2b835818fbae345
Gaurav Sutar <gauravstr05@gmail.com>
2023-01-17 19:54:03 +0530

    Map, Filter & Reduce in JavaScript

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-let num = [1, 2, 3, 4, 5, 6, 7, 8];
+/** create a new array by programming some operation on each array element */
+/*
+let arr = [1, 2, 3, 4, 5, 6, 7];
 
-for (let i in num) {
-  console.log(i);
-}
+ let a = arr.map((value, index, array) => {
+   console.log(value, index, array);
+   return value + index;
+ });
+
+console.log(a);
+*/
+
+// Array Filter method
+/*
+let arr2 = [10, 20, 30, 40, 50];
+let result = arr2.filter((value) => {
+  return value > 30;
+});
+console.log(result);
+*/
+
+// Array Reduce method
+let arr3 = [5, 10, 15, 20, 25, 30];
+
+let newArr3 = arr3.reduce((h1, h2) => {
+  return h1 + h2;
+});
+
+console.log(newArr3);
