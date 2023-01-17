ba6a1b9c3df2bb49b9642f15e44f31be6cf9c2d5
Gaurav Sutar <gauravstr05@gmail.com>
2023-01-17 12:23:26 +0530

    Javascript Array Methods

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-let marks = [80, 90, 50, 60, 75, 95, 98, 85, 80, null, "Not Present"];
+let num = [5, 10, 15, 20, 25, 50];
 
-marks[11] = 65; // adding a new value to the array
+let b = num.toString(); // b is now string
+//console.log(b);
 
-console.log(marks);
-console.log(marks.length); // to find indexes in array
+let c = num.join("_"); // join 2 array
+//console.log(c);
+
+let pop = num.pop(); // pop used for removing last elememt of an array
+//console.log(num, pop);
+
+let push = num.push(56); // adding element to the last index
+//console.log(num, push);
+
+let shift = num.shift(); // removing first element of an array
+//console.log(num, shift);
+
+let unShift = num.unshift("Gau"); // adding element to the first index of an array
+console.log(num, unShift);
